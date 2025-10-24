d497ea5bb99f86b0d3870a0df4b5550a5552c003
EvanMeyerss <evanpaulmeyers@gmail.com>
2025-10-24 11:44:31 -0500

    settings.gradle.kts upload

---
+++ settings.gradle.kts
@@ -0,0 +1,46 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val localProperties = Properties().apply {
+    val file = rootDir.resolve("local.properties")
+    if (file.exists()) {
+        load(FileInputStream(file))
+    }
+}
+
+val gprUser: String? = localProperties.getProperty("gpr.user")
+val gprKey: String? = localProperties.getProperty("gpr.key")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/aesirlab/annotations-repo")
+            credentials {
+                username = gprUser
+                password = gprKey
+            }
+        }
+
+    }
+}
+
+rootProject.name = "SolidConversion"
+include(":app")
+ 
